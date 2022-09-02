92f552932686012a71c8b563725ccb1634c95543
AdamSahlen93 <adam-sahlen@hotmail.com>
2022-09-02 11:11:23 +0200

    Mobile 1.0

--- js/global.js
+++ js/global.js
@@ -120,4 +120,22 @@ updateButton3.addEventListener('click', () => {
   }
 });
 
+function openNav() {
+  document.getElementById("myNav").style.height = "10rem";
+}
 
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+var activeNav = false;
+
+function hamburger() {
+  if (activeNav) {
+    closeNav()
+    activeNav = false
+  } else {
+    openNav()
+    activeNav = true
+  }
+}
